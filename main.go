b1cc2641e766fa225d69f2dfb81978e4c3447dd2
Emie0038 <Emie0038>
2023-07-20 17:11:43 +0200

    updates

--- main.go
+++ main.go
@@ -5,21 +5,34 @@ import "fmt"
 func main() {
 
 	// Declare variables and assign values using short declaration syntax (type inference)
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainningTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	var bookings [50]string //string{"Emie", "Gloria", "Anjelie", "Jojie", "Simona", "Wanesa"}
 
 	fmt.Printf("Hello, World!") // prints "Hello, world!" to the console
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainningTickets)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
 	// ask user for their name
-	var userTickets int
+	var lastName string
+	var emailAddress string
+	var userTickets uint
+	fmt.Println("Enter your firstname: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your lastname: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&emailAddress)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
 
-	userName = "Bill"
-	userTickets = 12
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("Thank you %v %v for booking %v tickets, and receipts will be send to your email %v.\n", firstName, lastName, userTickets, emailAddress)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
 }
